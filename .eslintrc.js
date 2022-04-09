40e7c32b34caaf3362e0d8e0f71015570cf349a8
maliut <lqn619@163.com>
2022-04-09 16:07:13 +0800

    Initial commit - webpack wrapper for rmmv projects

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true,
+    'es2021': true
+  },
+  // "globals": {
+  //   "var1": "writable",
+  //   "var2": "readonly"
+  // },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
